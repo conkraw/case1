7cde0b2bb7773ed62def78fba18516c67465119f
conkraw <conkraw@gmail.com>
2024-10-01 08:40:20 -0400

    Create session_management.py

---
+++ utils/session_management.py
@@ -0,0 +1,15 @@
+# utils/session_management.py
+
+import streamlit as st
+
+def collect_session_data():
+    session_data = {
+        "unique_code": st.session_state.get("unique_code", ""),
+        #"vs_data": st.session_state.get("vs_data", {}),
+        #"diagnoses": st.session_state.get("diagnoses", []),
+        # Add other session state variables as needed
+    }
+    return session_data
+
+
+
