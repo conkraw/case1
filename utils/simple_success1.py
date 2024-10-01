727810ef970e12e0066d6fbe7b3154e61f0766f6
conkraw <conkraw@gmail.com>
2024-10-01 09:04:04 -0400

    Create simple_success1.py

---
+++ utils/simple_success1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def display_simple_success1():
+    print("Displaying Simple Success page.")  # Debugging statement
+    st.title("Success!")
+    st.write("Your operation was successful1111.")
+    
